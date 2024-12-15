e7a82fd91ce4e26de446938611d649c418a19f99
Seif Hesham <popo.hisham.sh@gmail.com>
2024-12-15 22:10:56 +0200

    CardSeven

---
+++ CardSeven.cpp
@@ -0,0 +1,31 @@
+#include "CardSeven.h"
+
+
+CardSeven::CardSeven(const CellPosition& pos) : Card(pos) // set the cell position of the card
+{
+	cardNumber = 7; //sets the inherited cardNumber data member with the card number (7 here)
+}
+
+
+void CardSeven::ReadCardParameters(Grid* pGrid)
+{
+
+}
+
+void CardSeven::Apply(Grid* pGrid, Player* pPlayer)
+{
+	Card::Apply(pGrid, pPlayer);
+
+	pGrid->PrintErrorMessage("This card gives you another dice roll. Click to continue...");
+
+	int player_num = pPlayer->GetPlayerNumber();
+
+	for (int i = 0; i < 3; i++) pGrid->AdvanceCurrentPlayer();
+
+	pGrid->PrintErrorMessage("Player " + to_string(player_num) + " Got an extra dice roll, Click to continue...");
+}
+
+
+CardSeven::~CardSeven()
+{
+}
---
+++ CardSeven.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Card.h"
+
+/*
+ Card7:
+ ● Gives the player another dice roll.
+ ● Input data: None.
+*/
+
+class CardSeven : public Card
+{
+	//No card parameters
+
+public:
+	CardSeven(const CellPosition&); // A Constructor takes card position
+
+	virtual void ReadCardParameters(Grid*); // Reads nothing
+
+	virtual void Apply(Grid*, Player*); //Applies the effect of CardThree on passed player
+	//by giving passed player another dice roll  
+
+	virtual ~CardSeven(); // A Virtual Destructor
+};
