6a72c5c22857d149b20fb509142bd810954d79e1
Seif Hesham <popo.hisham.sh@gmail.com>
2024-12-18 23:49:46 +0200

    fixing errors

--- CardFour.cpp
+++ CardFour.cpp
@@ -15,16 +15,15 @@ void CardFour::Apply(Grid* pGrid, Player* pPlayer)
 	Card::Apply(pGrid, pPlayer);
 	pGrid->PrintErrorMessage("This card moves you to the next snake. Click to continue...");
 
-	Snake* next_snake = pGrid->GetNextSnake(this->GetPosition());
-
-	if (!next_snake)
-	{
-		pGrid->PrintErrorMessage("There is no snake ahead of you! Click to continue...");
-	}
-	else {
-		pGrid->UpdatePlayerCell(pPlayer, next_snake->GetPosition());
-		next_snake->Apply(pGrid, pPlayer);
-	}
+	//Snake* next_snake = pGrid->GetNextSnake(this->GetPosition());
+	//if (!next_snake)
+	//{
+	//	pGrid->PrintErrorMessage("There is no snake ahead of you! Click to continue...");
+	//}
+	//else {
+	//	pGrid->UpdatePlayerCell(pPlayer, next_snake->GetPosition());
+	//	next_snake->Apply(pGrid, pPlayer);
+	//}
 }
 
 CardFour::~CardFour()
